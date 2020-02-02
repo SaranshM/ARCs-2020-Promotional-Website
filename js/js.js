79e3b5a5280fee046f75a5ed9594a2c85df54e9f
Saransh Mehta <saranshmehta96@gmail.com>
2020-02-02 11:57:47 +0530

    exrdctfvygbuhn

--- js/js.js
+++ js/js.js
@@ -1,3 +1,5 @@
+/*Vertival timeline*/
+
 var ptr=document.getElementsByClassName('pointer');
 
 ptr[1].style.backgroundColor="rgb(210,210,210,0)";
@@ -27,6 +29,289 @@ function change(pic,same1,same2)
 	document.getElementsByClassName('about1')[same1].style.border="solid 13px black";
 	document.getElementsByClassName('about1')[same2].style.border="solid 13px black";
 }
+
+
+/*Horizontal timeline*/
+
+var pos=2;
+
+var right=document.getElementsByClassName('nav_right')[0];
+var left=document.getElementsByClassName('nav_left')[0];
+var event_name=document.getElementsByClassName('event_name');
+var event_time=document.getElementsByClassName('event_time');
+
+var left_pos=75;
+
+right.addEventListener('click',function(){
+	
+	if(pos==2)
+	{
+		event_name[pos].style.color="rgb(200,200,200)";
+		event_name[pos].style.fontSize="0.8vw";
+		event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="65px";
+		event_name[pos-1].style.marginRight="5%";
+
+		event_time[pos].style.color="rgb(200,200,200)";
+		event_time[pos].style.fontSize="1.1vw";
+		event_time[pos].style.fontWeight="normal";
+
+		pos=pos+1;
+
+		event_name[pos].style.color="rgb(0,0,0)";
+		event_name[pos].style.fontSize="1.3vw";
+		// event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="35px";
+		event_name[pos].style.marginRight="2%";	
+
+		event_time[pos].style.color="rgb(0,0,0)";
+		event_time[pos].style.fontSize="1.4vw";
+		event_time[pos].style.fontWeight="bold";
+		event_time[0].style.left="30.8%";
+		event_time[1].style.left="45.5%";
+		event_time[pos].style.left="74%";
+		event_time[2].style.left="61%";
+		event_time[4].style.left="86.5%";
+
+		
+	}
+	else if(pos==3)
+	{
+		event_name[pos].style.color="rgb(200,200,200)";
+		event_name[pos].style.fontSize="0.8vw";
+		event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="65px";
+		event_name[pos-1].style.marginRight="5%";
+
+		event_time[pos].style.color="rgb(200,200,200)";
+		event_time[pos].style.fontSize="1.1vw";
+		event_time[pos].style.fontWeight="normal";
+
+		pos=pos+1;
+
+		event_name[pos].style.color="rgb(0,0,0)";
+		event_name[pos].style.fontSize="1.3vw";
+		// event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="35px";
+		event_name[pos].style.marginRight="2%";	
+
+		event_time[pos].style.color="rgb(0,0,0)";
+		event_time[pos].style.fontSize="1.4vw";
+		event_time[pos].style.fontWeight="bold";
+		event_time[0].style.left="30.8%";
+		event_time[1].style.left="45.5%";
+		event_time[3].style.left="76%";
+		event_time[2].style.left="61%";
+		event_time[pos].style.left="88.5%";
+
+		
+	}
+
+	else if(pos==0)
+	{
+		event_name[pos].style.color="rgb(200,200,200)";
+		event_name[pos].style.fontSize="0.8vw";
+		event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="65px";
+		event_name[pos].style.marginRight="2.3%";
+
+		event_time[pos].style.color="rgb(200,200,200)";
+		event_time[pos].style.fontSize="1.1vw";
+		event_time[pos].style.fontWeight="normal";
+
+		pos=pos+1;
+
+		event_name[pos].style.color="rgb(0,0,0)";
+		event_name[pos].style.fontSize="1.3vw";
+		// event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="35px";
+		event_name[pos].style.marginRight="2%";	
+
+		event_time[pos].style.color="rgb(0,0,0)";
+		event_time[pos].style.fontSize="1.4vw";
+		event_time[pos].style.fontWeight="bold";
+		event_time[0].style.left="30.8%";
+		event_time[pos].style.left="43%";
+		event_time[3].style.left="71%";
+		event_time[2].style.left="56.5%";
+		event_time[4].style.left="86.5%";
+
+		
+	}
+
+	else if(pos==1)
+	{
+		event_name[pos].style.color="rgb(200,200,200)";
+		event_name[pos].style.fontSize="0.8vw";
+		event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="65px";
+		event_name[pos-1].style.marginRight="5%";
+
+		event_time[pos].style.color="rgb(200,200,200)";
+		event_time[pos].style.fontSize="1.1vw";
+		event_time[pos].style.fontWeight="normal";
+
+		pos=pos+1;
+
+		event_name[pos].style.color="rgb(0,0,0)";
+		event_name[pos].style.fontSize="1.3vw";
+		// event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="35px";
+		event_name[pos].style.marginRight="2%";	
+
+		event_time[pos].style.color="rgb(0,0,0)";
+		event_time[pos].style.fontSize="1.4vw";
+		event_time[pos].style.fontWeight="bold";
+		event_time[0].style.left="30.8%";
+		event_time[1].style.left="45.5%";
+		event_time[3].style.left="72%";
+		event_time[pos].style.left="58.5%";
+		event_time[4].style.left="86.5%";
+
+		
+	}
+
+
+
+
+
+});
+
+left.addEventListener('click',function(){
+
+	if(pos==2)
+	{
+		event_name[pos].style.color="rgb(200,200,200)";
+		event_name[pos].style.fontSize="0.8vw";
+		event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="65px";
+		event_name[pos-2].style.marginRight="2.3%";
+
+		event_time[pos].style.color="rgb(200,200,200)";
+		event_time[pos].style.fontSize="1.1vw";
+		event_time[pos].style.fontWeight="normal";
+
+		pos=pos-1;
+
+		event_name[pos].style.color="rgb(0,0,0)";
+		event_name[pos].style.fontSize="1.3vw";
+		// event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="35px";
+		event_name[pos].style.marginRight="2%";	
+
+		event_time[pos].style.color="rgb(0,0,0)";
+		event_time[pos].style.fontSize="1.4vw";
+		event_time[pos].style.fontWeight="bold";
+		event_time[0].style.left="30%";
+		event_time[pos].style.left="43%";
+		event_time[3].style.left="71%";
+		event_time[2].style.left="55.5%";
+		event_time[4].style.left="86.5%";
+
+		
+	}
+	else if(pos==1)
+	{
+		event_name[pos].style.color="rgb(200,200,200)";
+		event_name[pos].style.fontSize="0.8vw";
+		event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="65px";
+		event_name[pos-1].style.marginRight="5%";
+
+		event_time[pos].style.color="rgb(200,200,200)";
+		event_time[pos].style.fontSize="1.1vw";
+		event_time[pos].style.fontWeight="normal";
+
+		pos=pos-1;
+
+		event_name[pos].style.color="rgb(0,0,0)";
+		event_name[pos].style.fontSize="1.3vw";
+		// event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="35px";
+		event_name[pos].style.marginRight="2%";	
+
+		event_time[pos].style.color="rgb(0,0,0)";
+		event_time[pos].style.fontSize="1.4vw";
+		event_time[pos].style.fontWeight="bold";
+		event_time[pos].style.left="27.8%";
+		event_time[1].style.left="41%";
+		event_time[3].style.left="71.5%";
+		event_time[2].style.left="56.5%";
+		event_time[4].style.left="86.5%";
+
+		event_name[pos].style.marginRight="-1.5%";
+		event_name[pos].style.left="10%";
+		event_name[1].style.marginRight="5%";
+		event_name[2].style.marginRight="5%";
+
+		
+	}
+
+	else if(pos==4)
+	{
+		event_name[pos].style.color="rgb(200,200,200)";
+		event_name[pos].style.fontSize="0.8vw";
+		event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="65px";
+		event_name[pos-2].style.marginRight="2.3%";
+
+		event_time[pos].style.color="rgb(200,200,200)";
+		event_time[pos].style.fontSize="1.1vw";
+		event_time[pos].style.fontWeight="normal";
+
+		pos=pos-1;
+
+		event_name[pos].style.color="rgb(0,0,0)";
+		event_name[pos].style.fontSize="1.3vw";
+		// event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="35px";
+		event_name[pos].style.marginRight="2%";	
+
+		event_time[pos].style.color="rgb(0,0,0)";
+		event_time[pos].style.fontSize="1.4vw";
+		event_time[pos].style.fontWeight="bold";
+		event_time[0].style.left="30%";
+		event_time[1].style.left="45.5%";
+		event_time[pos].style.left="73%";
+		event_time[2].style.left="61%";
+		event_time[4].style.left="86%";
+
+		
+	}
+	else if(pos==3)
+	{
+		event_name[pos].style.color="rgb(200,200,200)";
+		event_name[pos].style.fontSize="0.8vw";
+		event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="65px";
+		event_name[pos-2].style.marginRight="2.3%";
+
+		event_time[pos].style.color="rgb(200,200,200)";
+		event_time[pos].style.fontSize="1.1vw";
+		event_time[pos].style.fontWeight="normal";
+
+		pos=pos-1;
+
+		event_name[pos].style.color="rgb(0,0,0)";
+		event_name[pos].style.fontSize="1.3vw";
+		// event_name[pos].style.left="13.5%";
+		event_name[pos].style.top="35px";
+		event_name[pos].style.marginRight="2%";
+		event_name[pos+1].style.marginRight="5%";	
+
+		event_time[pos].style.color="rgb(0,0,0)";
+		event_time[pos].style.fontSize="1.4vw";
+		event_time[pos].style.fontWeight="bold";
+		event_time[0].style.left="30%";
+		event_time[1].style.left="45.5%";
+		event_time[3].style.left="71.5%";
+		event_time[pos].style.left="58.5%";
+		event_time[4].style.left="86%";
+
+		
+	}
+	
+});
 
 
 
